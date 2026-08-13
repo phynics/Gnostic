8c420b593e3271bb831dd1b7eaaaa51dfd950581
atakan <contact@atkn.me>
2026-08-13 22:07:34 +0200

    build: enforce warning-free builds

--- .devcontainer/resolve.sh
+++ .devcontainer/resolve.sh
@@ -4,4 +4,4 @@ test -f /workspace/Package.swift || {
     echo "Package.swift is not present on this branch" >&2
     exit 2
 }
-swift package resolve --cache-path /workspace/.swiftpm-cache
+swift package resolve --quiet --cache-path /workspace/.swiftpm-cache
