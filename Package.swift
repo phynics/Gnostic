b235b86ded929c5870a8f3729213c447dc7b2eb3
atakan <contact@atkn.me>
2026-09-09 01:58:59 +0200

    chore(deps): update PositronicKit to 5.1.0 and swift-log to 1.15.1

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/phynics/Axoloty.git", exact: "0.7.0"),
-        .package(url: "https://github.com/phynics/PositronicKit.git", exact: "5.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.15.0"),
+        .package(url: "https://github.com/phynics/PositronicKit.git", exact: "5.1.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.15.1"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.8.2"),
     ],
     targets: [
